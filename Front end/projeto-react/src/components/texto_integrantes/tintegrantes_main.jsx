174cba44d9ddb805de091bb267c2feb1f2e3fc90
isacostaf <iisabellecostaf@gmail.com>
2024-08-07 16:09:18 -0300

    Layout fotos

--- Front end/projeto-react/src/components/texto_integrantes/tintegrantes_main.jsx
+++ Front end/projeto-react/src/components/texto_integrantes/tintegrantes_main.jsx
@@ -4,9 +4,45 @@ const TIntegrantes = () => {
     return (
         <div id="Participantes">
             <Container className="inicio">
-                <p className="about_text_title">Quem somos nós?</p>
-                <p className="about_text"><br/>Somos alunos de Engenharia de Software da Universidade de Brasília cursando a disciplina de Métodos de Desenvolvimento de Software ensinada pela docente Carla Rocha.
-Temos um prazer imenso em apresentar esse projeto criado por todos nós no intuito de facilitar e ajudar nossos usuários</p>
+                <div className = "firstline">
+                    <img className="imagem1_0" src="https://avatars.githubusercontent.com/u/76632983?v=4"/>
+                    <img className="imagem1_2" src="https://avatars.githubusercontent.com/u/76632983?v=4"/>
+                    <img className="imagem1_3" src="https://avatars.githubusercontent.com/u/76632983?v=4"/>
+                    <img className="imagem1_4" src="https://avatars.githubusercontent.com/u/76632983?v=4"/>
+                </div>
+                <div className = "secondline">
+                    <img className="imagem2_0" src="https://avatars.githubusercontent.com/u/76632983?v=4"/>
+                    <img className="imagem2_2" src="https://avatars.githubusercontent.com/u/76632983?v=4"/>
+                    <img className="imagem2_3" src="https://avatars.githubusercontent.com/u/76632983?v=4"/>              
+                </div>
+                <div className="centerline">
+                    <div className="esquerda">
+                        <img className="imagem3_0" src="https://avatars.githubusercontent.com/u/76632983?v=4"/>
+                        <img className="imagem3_1" src="https://avatars.githubusercontent.com/u/76632983?v=4"/>
+                    </div>
+                    <div className="textos">
+                        <p className="about_text_title">Quem somos nós?</p>
+                        <p className="about_text"><br/>Somos alunos de Engenharia de Software da Universidade de Brasília cursando a disciplina de Métodos de Desenvolvimento de Software ensinada pela docente Carla Rocha.
+                        Temos um prazer imenso em apresentar esse projeto criado por todos nós no intuito de facilitar e ajudar nossos usuários</p>
+                    </div>
+                    <div className="direita">
+                        
+                        <img className="imagem3_3" src="https://avatars.githubusercontent.com/u/76632983?v=4"/>
+                    </div>
+                </div>
+                <div className = "fourthline">
+                    <img className="imagem2_0" src="https://avatars.githubusercontent.com/u/76632983?v=4"/>
+                    <img className="imagem2_2" src="https://avatars.githubusercontent.com/u/76632983?v=4"/>
+                    <img className="imagem2_3" src="https://avatars.githubusercontent.com/u/76632983?v=4"/>              
+                </div>
+                <div className = "fifthline">
+                    <img className="imagem1_0" src="https://avatars.githubusercontent.com/u/76632983?v=4"/>
+                    <img className="imagem1_2" src="https://avatars.githubusercontent.com/u/76632983?v=4"/>
+                    <img className="imagem1_3" src="https://avatars.githubusercontent.com/u/76632983?v=4"/>
+                    <img className="imagem1_4" src="https://avatars.githubusercontent.com/u/76632983?v=4"/>
+                </div>
+                
+                
             </Container>
         </div>
     )
